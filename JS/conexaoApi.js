554f11626bd64a62047f5a6bdbfba1f5db0e949d
EdoardaDepine <edoarda.depine@gmail.com>
2023-04-12 09:17:21 -0300

    implementando requisição do tipo POST

--- JS/conexaoApi.js
+++ JS/conexaoApi.js
@@ -4,3 +4,21 @@ export default async function getVideoList() {
 
   return videoList;
 }
+
+export default async function createdVideo(titulo, descricao, url, imagem) {
+  const conectionAPI = await fetch("http://localhost:3000/videos", {
+    method: "POST",
+    headers: {
+      "Content-type": "aplication/json",
+    },
+    body: JSON.stringify({
+      titulo: titulo,
+      descricao: `${descricao} mil visualizaões`,
+      url: url,
+      imagem: imagem,
+    }),
+  });
+
+  const conectionAPIconverter = conectionAPI.json();
+  return conectionAPIconverter;
+}
